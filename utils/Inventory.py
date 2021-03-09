f5b756a4b80e31e7e6c5ff7254a686ccae440c5e
Buczkek <99tuba99@gmail.com>
2021-03-09 15:05:55 +0100

    Add some basic methods to Inventory and Object classes. Beginning of objects relation oriented features.

--- utils/Inventory.py
+++ utils/Inventory.py
@@ -1,3 +1,21 @@
-class Inventory:  # TODO Inventory class
-    def __init__(self):
+from Object import Object
+
+
+class Inventory(Object):  # TODO Inventory class
+    def __init__(self, size=5):
         self._Items = []
+        self._Size = size
+        self._Empty = True
+        self._Occupied = 0
+        self._Pos = self._Bound.get_pos()
+
+    def get_items(self):
+        return self._Items
+
+    def insert_item(self, index, item):
+        if self._Items[index]:
+            self.drop_item()
+            return self.insert_item(index, item)
+
+    def drop_item(self):
+        raise NotImplementedError
--- utils/Object.py
+++ utils/Object.py
@@ -1,10 +1,19 @@
 class Object:  # TODO Object class
-    def __init__(self, x=0, y=0, z=0):
-        self._x = 0
-        self._y = 0
-        self._z = 0
+    def __init__(self, x=0, y=0, z=0, *, boundable=False, bound: 'Object' = None):
+        self._Pos = [x, y, z]
         self._Interactable = True
-        self._Texture = None
+        self._Texture = None  # TODO Set default texture (image)
+        if bound and not boundable:
+            raise Exception("Bound given although boundable set to False")
+        self._Boundable = boundable
+        self._Bound = bound
 
     def draw(self, surface):  # TODO ...
         pass
+
+    def get_pos(self):
+        return tuple(self._Pos)
+
+    def _get_bound_pos(self):
+        return self._Bound.get_pos()
+
