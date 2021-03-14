ce8219dec9913ef061325e45270cfa2639a51e53
Buczkek <99tuba99@gmail.com>
2021-03-14 01:03:14 +0100

    Every Object will be boundable from now on Implementing Config class

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
-class Config:
+import sys, time
+
+OPTIONS = {
+    "": "",
+}
+
+
+class Config:  # TODO check for bugs
     def __init__(self, path="config.cfg"):
         self._path = path
+        self._settings = {}
         if not self.check_existence():
             self.config_creator()
-        else:
-            self.config_import()
-        # TODO save
-        # TODO config fill default
+            self.load_default()
+        elif not self.config_import():
+            self.load_default()
+        self.config_import()
+        if len(self._settings) < len(OPTIONS):
+            self.fill_settings()
 
     def check_existence(self):
         try:
@@ -17,11 +27,43 @@ class Config:
         except FileNotFoundError:
             return False
 
-    def config_creator(self):  # TODO
-        pass
+    def config_creator(self):
+        try:
+            with open(self._path, 'w') as file:
+                for (option, setting) in OPTIONS:
+                    file.write(f"{option}={setting}\n")
+        except:
+            pass
 
-    def config_import(self):  # TODO
-        pass
+    def load_default(self):
+        for (option, setting) in OPTIONS:
+            self._settings[option] = setting
 
-    def config_save(self):  # TODO
-        pass
+    def config_import(self):
+        try:
+            with open(self._path, 'r') as config_file:
+                for line in config_file.readline(len(OPTIONS)):
+                    option, setting = line.strip().split('=')
+                    if option in OPTIONS:
+                        self._settings[option] = setting
+            return True
+        except:
+            return False
+
+    def config_save(self, path=""):
+        if not path:
+            path = self._path
+        try:
+            with open(path, 'w') as config_file:
+                for (option, setting) in self._settings:
+                    config_file.write(f"{option}={setting}\n")
+        except:
+            pass
+
+    def get_setting(self, setting):
+        return self._settings[setting]
+
+    def fill_settings(self):
+        for (option, setting) in OPTIONS:
+            if option not in self._settings:
+                self._settings[option] = setting
--- utils/Object.py
+++ utils/Object.py
@@ -1,11 +1,8 @@
 class Object:  # TODO Object class
-    def __init__(self, x=0, y=0, z=0, *, boundable=False, bound: 'Object' = None):
+    def __init__(self, x=0, y=0, z=0, *, bound: 'Object' = None):
         self._Pos = [x, y, z]
         self._Interactable = True
         self._Texture = None  # TODO Set default texture (image)
-        if bound and not boundable:
-            raise Exception("Bound given although boundable set to False")
-        self._Boundable = boundable
         self._Bound = bound
 
     def draw(self, surface):  # TODO ...
